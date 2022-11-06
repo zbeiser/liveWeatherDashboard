7aca35570743d84479d5d7d4272b146c600fb7bf
Zach Beiser <zbeiser@gmail.com>
2022-11-06 12:20:27 -0800

    added function to update searches from local storage to create buttons on load. Added icon functionality to current weather

--- Assets/script.js
+++ Assets/script.js
@@ -9,10 +9,13 @@ var APIKey = "6eeea499c62fe1e384fc56e6dc479df1";
 
 var forecastArray = [];
 
+searchButtonEl.addEventListener('click', getApi);
+
 function getApi() {
   var cityName = citySearchEl.value
   var geocodeUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
+    "http://api.openweathermap.org/geo/1.0/direct?q=" 
+    + cityName + "&limit=1&appid=" + APIKey;
 
   fetch(geocodeUrl)
     .then(function (response) {
@@ -23,9 +26,11 @@ function getApi() {
       var longitude = data[0].lon;
 
       var cityCurrentUrl =
-        "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
+        "https://api.openweathermap.org/data/2.5/weather?lat=" 
+        + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
       var cityFiveDayUrl =
-        "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
+        "https://api.openweathermap.org/data/2.5/forecast?lat=" 
+        + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
 
       fetch(cityCurrentUrl)
         .then(function (response) {
@@ -40,9 +45,16 @@ function getApi() {
           cityWindEl.innerText = "Wind: " + data.wind.speed + " MPH";
           cityHumidityEl.innerText = "Humidity: " + data.main.humidity + " %";
 
+          var currentCityIcon = document.createElement("img");
+          currentCityIcon.setAttribute("src", "http://openweathermap.org/img/wn/" 
+          + data.weather[0].icon + "@2x.png");
+          currentCityIcon.setAttribute("style", "scale:50%");
+          cityNameDateEl.appendChild(currentCityIcon);
+
           var savedCity = {
             cityName: data.name,
             cityDate: "(" + date + ")",
+            cityIcon: "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png",
             temp: cityTempEl.innerText,
             wind: cityWindEl.innerText,
             humidity: cityHumidityEl.innerText
@@ -106,31 +118,44 @@ function getApi() {
           forecastArray.push(savedFiveDay);
           localStorage.setItem("Forecasts", JSON.stringify(forecastArray));
         });
-      console.log(forecastArray);
-    })
+    });
 }
 
 function retrieveSearch(ev) {
   var clickedBtn = ev.target.innerText;
   for (i = 0; i < forecastArray.length; i++) {
-    if (forecastArray[i].cityName == clickedBtn && Object.keys(forecastArray[i]).length == 5) {
+    if (forecastArray[i].cityName == clickedBtn && Object.keys(forecastArray[i]).length == 6) {
       cityNameDateEl.innerText = forecastArray[i].cityName + " " + forecastArray[i].cityDate;
       cityTempEl.innerText = forecastArray[i].temp;
       cityWindEl.innerText = forecastArray[i].wind;
       cityHumidityEl.innerText = forecastArray[i].humidity;
+
+      if (currentCityIcon) {
+        currentCityIcon.src = forecastArray[i].cityIcon;
+      } else {
+        var currentCityIcon = document.createElement("img");
+          currentCityIcon.setAttribute("src", forecastArray[i].cityIcon);
+          currentCityIcon.setAttribute("style", "scale:50%");
+          cityNameDateEl.appendChild(currentCityIcon);
+      }
     } else if (forecastArray[i].cityName == clickedBtn && Object.keys(forecastArray[i]).length == 21) {
+      forecastCardEl[0].children[0].innerText = forecastArray[i].dateOne;
       forecastCardEl[0].children[2].children[0].innerText = forecastArray[i].tempOne;
       forecastCardEl[0].children[2].children[1].innerText = forecastArray[i].windOne;
       forecastCardEl[0].children[2].children[2].innerText = forecastArray[i].humOne;
+      forecastCardEl[1].children[0].innerText = forecastArray[i].dateTwo;
       forecastCardEl[1].children[2].children[0].innerText = forecastArray[i].tempTwo;
       forecastCardEl[1].children[2].children[1].innerText = forecastArray[i].windTwo;
       forecastCardEl[1].children[2].children[2].innerText = forecastArray[i].humTwo;
+      forecastCardEl[2].children[0].innerText = forecastArray[i].dateThree;
       forecastCardEl[2].children[2].children[0].innerText = forecastArray[i].tempThree;
       forecastCardEl[2].children[2].children[1].innerText = forecastArray[i].windThree;
       forecastCardEl[2].children[2].children[2].innerText = forecastArray[i].humThree;
+      forecastCardEl[3].children[0].innerText = forecastArray[i].dateFour;
       forecastCardEl[3].children[2].children[0].innerText = forecastArray[i].tempFour;
       forecastCardEl[3].children[2].children[1].innerText = forecastArray[i].windFour;
       forecastCardEl[3].children[2].children[2].innerText = forecastArray[i].humFour;
+      forecastCardEl[4].children[0].innerText = forecastArray[i].dateFive;
       forecastCardEl[4].children[2].children[0].innerText = forecastArray[i].tempFive;
       forecastCardEl[4].children[2].children[1].innerText = forecastArray[i].windFive;
       forecastCardEl[4].children[2].children[2].innerText = forecastArray[i].humFive;
@@ -138,6 +163,24 @@ function retrieveSearch(ev) {
   }
 }
 
-// TODO: Display icons; Update buttons and forecastArray from localStorage on reload;
+function updateSearches() {
+  var loadForecasts = JSON.parse(localStorage.getItem('Forecasts'));
 
-searchButtonEl.addEventListener('click', getApi);
+  if (loadForecasts) {
+    forecastArray = loadForecasts;
+  }
+
+  for (i = 0; i < forecastArray.length; i++) {
+    if (Object.keys(forecastArray[i]).length == 6) {
+      var savedCityBtnEl = document.createElement("button");
+      savedCityBtnEl.innerText = forecastArray[i].cityName;
+      savedCityBtnEl.classList.add("btn", "btn-secondary", "btn-block")
+      document.getElementById("searchColumn").appendChild(savedCityBtnEl);
+      savedCityBtnEl.addEventListener("click", retrieveSearch)
+    }
+  }
+}
+
+updateSearches();
+
+// TODO: Display icons;
