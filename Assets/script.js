9be3770b750cecef1514eab2506ccfe50a0f9a30
Zach Beiser <zbeiser@gmail.com>
2022-11-07 17:24:09 -0800

    wrote the README and JS comments to show work

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
+// Declare DOM elements
 var searchButtonEl = document.getElementById('searchBtn');
 var citySearchEl = document.getElementById('citySearch');
 var cityNameDateEl = document.getElementById('cityNameDate');
@@ -5,9 +6,14 @@ var cityTempEl = document.getElementById('cityTemp');
 var cityWindEl = document.getElementById('cityWind');
 var cityHumidityEl = document.getElementById('cityHum');
 var forecastCardEl = document.getElementsByClassName('forecastCard');
+
+// Declare API key as variable
 var APIKey = "6eeea499c62fe1e384fc56e6dc479df1";
+
+// Declare today's date as variable for a comparison in the five day forecast function
 var currentDate = new Date().toLocaleDateString("en-US");
 
+// Declare global array for weather forecast data
 var forecastArray = [];
 
 searchButtonEl.addEventListener('click', getApi);
@@ -18,6 +24,7 @@ function getApi() {
     "https://api.openweathermap.org/geo/1.0/direct?q="
     + cityName + "&limit=1&appid=" + APIKey;
 
+  // First fetch utilizes user input of a city's name to store its latitude and longitude
   fetch(geocodeUrl)
     .then(function (response) {
       return response.json();
@@ -26,6 +33,7 @@ function getApi() {
       var latitude = data[0].lat;
       var longitude = data[0].lon;
 
+      // Creates both the current weather and five day forecast api URLs with the lat and long
       var cityCurrentUrl =
         "https://api.openweathermap.org/data/2.5/weather?lat="
         + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
@@ -33,11 +41,13 @@ function getApi() {
         "https://api.openweathermap.org/data/2.5/forecast?lat="
         + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
 
+      // Second fetch retrieves the current weather
       fetch(cityCurrentUrl)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
+          // Assigns the current weather data values to their respective elements
           var date = new Date(data.dt * 1000);
           date = date.toLocaleDateString("en-US");
 
@@ -46,12 +56,15 @@ function getApi() {
           cityWindEl.innerText = "Wind: " + data.wind.speed + " MPH";
           cityHumidityEl.innerText = "Humidity: " + data.main.humidity + " %";
 
+          // Creates the current weather icon
           var currentCityIcon = document.createElement("img");
           currentCityIcon.setAttribute("src", "https://openweathermap.org/img/wn/"
             + data.weather[0].icon + "@2x.png");
           currentCityIcon.setAttribute("style", "scale:50%");
           cityNameDateEl.appendChild(currentCityIcon);
 
+          // Assigns current weather data values to an object and stores it into an array,
+          // then sets that array into the localStorage
           var savedCity = {
             cityName: data.name,
             cityDate: "(" + date + ")",
@@ -63,6 +76,7 @@ function getApi() {
           forecastArray.push(savedCity);
           localStorage.setItem("Forecasts", JSON.stringify(forecastArray));
 
+          // Creates a button for every city search, adds event listener to retrieve their data
           var savedCityBtnEl = document.createElement("button");
           savedCityBtnEl.innerText = data.name;
           savedCityBtnEl.classList.add("btn", "btn-secondary", "btn-block")
@@ -70,6 +84,7 @@ function getApi() {
           savedCityBtnEl.addEventListener("click", retrieveSearch)
         });
 
+      // Last fetch retrieves the five day forecast
       fetch(cityFiveDayUrl)
         .then(function (response) {
           return response.json();
@@ -77,12 +92,15 @@ function getApi() {
         .then(function (data) {
           var fiveDayArray = [];
 
+          // Out of the 40 list items that contain 3-hour intervals of weather, this targets the 
+          // same hour of each day and makes sure it's not the current day. Stores 5 days into array.
           for (i = 0; i < data.list.length; i++) {
             if (data.list[i].dt_txt.includes("18:00:00") && new Date(data.list[i].dt * 1000) !== currentDate) {
               fiveDayArray.push(data.list[i]);
             }
           }
 
+          // Assigns the five day forecast data values & icons to their respective cards. 
           for (i = 0; i < fiveDayArray.length; i++) {
             var date = new Date(fiveDayArray[i].dt * 1000);
             date = date.toLocaleDateString("en-US");
@@ -98,7 +116,9 @@ function getApi() {
             forecastCardEl[i].children[2].children[2].innerText = "Humidity: "
               + fiveDayArray[i].main.humidity + " %";
           }
-          // Store data into local storage
+
+          // Assigns five day forecast data values to an object and stores it into an array,
+          // then sets that array into the localStorage.
           var savedFiveDay = {
             cityName: data.city.name,
             dateOne: forecastCardEl[0].children[0].innerText,
@@ -129,15 +149,17 @@ function getApi() {
           }
           forecastArray.push(savedFiveDay);
           localStorage.setItem("Forecasts", JSON.stringify(forecastArray));
-
-          console.log(data);
-          console.log(fiveDayArray);
         });
     });
 }
 
+// Upon clicking saved city buttons, this function retrieves & displays their data.
 function retrieveSearch(ev) {
+  // This saves the city's name to target the correct objects.
   var clickedBtn = ev.target.innerText;
+
+  // This targets current weather objects since they have a length of 6. Then updates current
+  // weather elements accordingly.
   for (i = 0; i < forecastArray.length; i++) {
     if (forecastArray[i].cityName == clickedBtn && Object.keys(forecastArray[i]).length == 6) {
       cityNameDateEl.innerText = forecastArray[i].cityName + " " + forecastArray[i].cityDate;
@@ -153,6 +175,8 @@ function retrieveSearch(ev) {
         currentCityIcon.setAttribute("style", "scale:50%");
         cityNameDateEl.appendChild(currentCityIcon);
       }
+      // This targets five day forecast objects since they have a length of 26. Then updates the
+      // five day forecast cards accordingly.
     } else if (forecastArray[i].cityName == clickedBtn && Object.keys(forecastArray[i]).length == 26) {
       forecastCardEl[0].children[0].innerText = forecastArray[i].dateOne;
       forecastCardEl[0].children[1].src = forecastArray[i].iconOne;
@@ -188,6 +212,8 @@ function retrieveSearch(ev) {
   }
 }
 
+// Upon webpage load, updates the forecast array if there's data saved in the localStorage.
+// Then recreates the saved city buttons by targeting city names in each current weather object.
 function updateSearches() {
   var loadForecasts = JSON.parse(localStorage.getItem('Forecasts'));
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # liveWeatherDashboard
+
+## Description
+The goal of this project was to create an application that can search for cities by name and display their current weather data, as well as a 5 day forecast. This involved the usage of the OpenWeatherMap API, which provided 3 useful API call links with a free subscription. The first is a geocoding API, which allows you to enter the name of a city and receive its latitude and longitude. This incorporates into the usage of the other two API urls, the Current Weather and 5 Day Forecast, since they both require latitude and longitude values for each search. With JavaScript fetches, I was able to store the weather data into arrays and update the UI with their values. Then using local storage, I was able to create persistent buttons that retrieve previously searched weather data and display it once again onto the UI.
+
+## Installation
+N/A
+
+## Usage
+Website's URL: https://zbeiser.github.io/liveWeatherDashboard/
+
+Open to application and type in the name of a city, then press search. You should be able to view the current weather in a large card on top, as well as a 5-day forecast in 5 smaller cards below. Upon searching for a city, a button with the name of the city will be created below the search button. You can click these buttons to view the data for that city again. There are icons that display the weather conditions, as well as lists displaying the temperature, wind, and humidity.
+
+![Screenshot](https://github.com/zbeiser/liveWeatherDashboard/blob/main/Assets/liveWeatherDashboard.png?raw=true)
+
+## Credits
+Thank you to the UW Coding Bootcamp staff and students, as well as W3Schools, Mozilla MDN Web Docs, and Stack Overflow for providing numerous educational resources during the development of this project. Thank you to the OpenWeatherMap API for providing the cities' locational and weather data for this project.
+
+## License
+Please refer to the LICENSE in the repo.
